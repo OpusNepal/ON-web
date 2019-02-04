8b03e84a2dce7f024da3134d1f7fea17de31cb57
Bikram Adhikari <bikram.adhikari@pdc01.lisnepal.com.np>
2019-02-04 13:56:14 +0545

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  api: "http://127.0.0.1:8848/api/",
-  files : "http://localhost:8848/",
+  api: "http://192.168.50.110:8848/api/",
+  files : "http://192.168.50.110:8848/",
   //api: "https://artysnepal.herokuapp.com/api/",
-  public: "http://127.0.0.1:8848/"
+  public: "http://192.168.50.110:8848/"
 
 };
 
