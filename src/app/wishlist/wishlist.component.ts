eea32701188e8cc94a16f994c0414dd4c1e8ce8d
yudeep <calm.yudeep@gmail.com>
2019-03-18 21:47:26 +0545

    customart and mydelivey

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -4,6 +4,8 @@ import { WishlistProduct } from '../app-models/wishlist-product';
 import { LocalStorageService } from '../auth/local-storage.service';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-wishlist',
@@ -14,7 +16,10 @@ export class WishlistComponent implements OnInit {
 
   products: WishlistProduct[]
 
-  constructor(private userService: UserService, private localStorageService: LocalStorageService, private router: Router) { }
+  constructor(public ratingConfig: NgbRatingConfig,private userService: UserService, private localStorageService: LocalStorageService, private router: Router) { 
+    ratingConfig.max = 5;
+
+  }
 
   ngOnInit() {
     if (this.localStorageService.getAuthData()) {
@@ -53,6 +58,12 @@ export class WishlistComponent implements OnInit {
     });
   }
 
+  rateChanged(newRating: Number, productId: Number) {
+    const { userId } = this.localStorageService.getAuthData();
+    this.userService.rateProduct(newRating, productId, userId).subscribe((res) => {
+      //console.log(res)
+    });
+  }
   viewProductDetail(productId: Number) {
     // navigate to the profile page
     this.router.navigate(['product-view'], { queryParams: { productId }});
