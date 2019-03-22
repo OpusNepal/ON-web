e4cb22dd40f9947ee00c9e2e895db0fbae2577a7
OpusNepal <47470421+OpusNepal@users.noreply.github.com>
2019-03-22 14:36:04 +0545

    update to https and domain name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  api: "http://68.183.82.137:8848/api/",
-  files : "http://68.183.82.137:8848/",
+  api: "https://www.opusnepal.com:8848/api/",
+  files : "https://www.opusnepal.com:8848/",
   // api: "https://artysnepal.herokuapp.com/api/",
-  public: "http://68.183.82.137:8848/"
+  public: "https://www.opusnepal.com:8848/"
 
 };
